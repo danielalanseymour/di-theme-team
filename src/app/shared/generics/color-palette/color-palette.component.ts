b9d6ce0dce956af5353c3cbfccdf4739555e458f
Seymour, Dan <Dan.Seymour@garmin.com>
2022-10-20 14:37:46 -0500

    Started Color palette

---
+++ src/app/shared/generics/color-palette/color-palette.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-color-palette',
+  styleUrls: ['./color-palette.component.scss'],
+  templateUrl: './color-palette.component.html'
+})
+export class ColorPaletteComponent {
+  name = "TEST";
+
+  constructor() {
+  }
+}
--- src/app/shared/generics/index.ts
+++ src/app/shared/generics/index.ts
@@ -1,1 +1,2 @@
 export * from './hover-details/hover-details.component';
+export * from './color-palette/color-palette.component';
